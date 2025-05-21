7006109ea0472b6a69550ccc27bcef648dc6fe45
MERALTIAN <obandojeremy52@gmail.com>
2025-05-21 17:07:07 -0600

    Hola

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import rutasCompras from './routes/compras.routes.js'
 import rutasEmpleados from './routes/empleados.routes.js'
 import rutasDetallesVentas from './routes/detalles_ventas.routes.js';
 import rutasDetallesCompras from'./routes/detalles_compras.routes.js';
+import rutasEstadisticas from './routes/estadisticas.routes.js';
 
 const app = express();
 
@@ -29,6 +30,7 @@ app.use('/api', rutasCompras);
 app.use('/api', rutasEmpleados);
 app.use('/api', rutasDetallesVentas);
 app.use('/api', rutasDetallesCompras);
+app.use('/api', rutasEstadisticas);
 
 app.use(express.json({ limit: '10mb' })); // Aumenta a 10 MB
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
