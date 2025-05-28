0a8bddae65f30783b7ca5e6110b7b54d86c9c61c
MERALTIAN <obandojeremy52@gmail.com>
2025-05-28 16:55:38 -0600

    nose nada

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import rutasEmpleados from './routes/empleados.routes.js'
 import rutasDetallesVentas from './routes/detalles_ventas.routes.js';
 import rutasDetallesCompras from'./routes/detalles_compras.routes.js';
 import rutasEstadisticas from './routes/estadisticas.routes.js';
+import rutasIA from './routes/ia.routes.js';
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use('/api', rutasEmpleados);
 app.use('/api', rutasDetallesVentas);
 app.use('/api', rutasDetallesCompras);
 app.use('/api', rutasEstadisticas);
+app.use('/ia', rutasIA);
 
 app.use(express.json({ limit: '10mb' })); // Aumenta a 10 MB
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
